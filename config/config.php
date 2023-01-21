9aabc605394857efa0e05120f3047de167b68695
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-21 21:58:41 +0600

    config file modified

--- config/config.php
+++ config/config.php
@@ -1,22 +1,26 @@
 <?php
 
+try {
+    //host 
+    $host = "localhost";
 
-//host 
-$host = "localhost";
+    //dbname
+    $dbname = "cleanblog";
 
-//dbname
-$dbname = "cleanblog";
+    //username
+    $username = "root";
 
-//username
-$username = "root";
+    //password
+    $pass = "";
 
-//password
-$pass = "";
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $pass);
-
-if ($conn == true) {
-    echo "connection established";
-} else {
-    echo "connection failed";
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
+
+// if ($conn == true) {
+//     echo "connection established";
+// } else {
+//     echo "connection failed";
+// }
