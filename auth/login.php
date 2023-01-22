58eb0906e5ecb10a2ea728bb1b9b3c8be526f968
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-22 10:52:09 +0600

    login file modified

--- auth/login.php
+++ auth/login.php
@@ -1,139 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require '../includes/header.php'  ?>
+<?php require '../config/config.php' ?>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Clean Blog - Start Bootstrap Theme</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
-        type="text/css" />
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-        rel="stylesheet" type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/styles.css" rel="stylesheet" />
-</head>
+<?php
 
-<body>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="../index.html">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="../index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="../posts/create.html">create</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="login.html">login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="register.html">register</a>
-                    </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="../contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Page Header-->
-    <header class="masthead" style="background-image: url('../assets/img/home-bg.jpg')">
-        <div class="container position-relative px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <div class="site-heading">
-                        <h1>Clean Blog</h1>
-                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+//check fo the submit
+if (isset($_POST['submit'])) {
+  if ($_POST['email'] == '' or $_POST['password'] == '') {
+    echo '<div class="alert alert-danger" role="alert">Please fill out all the required fields.</div>';
+    header('location:../index.php');
+  } else {
+    //take the data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-    <!-- Main Content-->
-    <div class="container px-4 px-lg-5">
-        <form method="POST" action="login.php">
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
-            </div>
+    //write our query string
+    $login = $conn->query("SELECT * FROM users WHERE email = '$email' ");
 
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
-            </div>
+    //execute the query and then fetch the results
+    $login->execute();
 
-            <!-- Submit button -->
-            <button type="submit" name="submit" class="btn btn-primary mb-4 text-center">
-                Login
-            </button>
 
-            <!-- Register buttons -->
-            <div class="text-center">
-                <p>
-                    a new member? Create an acount<a href="register.php"> Register</a>
-                </p>
-            </div>
-        </form>
+    $row = $login->fetch(PDO::FETCH_ASSOC);
+
+    //do our row Count and
+    if ($login->rowCount() > 0) {
+
+      //to do our password verify + redirect to index page
+      if (password_verify($password, $row['mypassword'])) {
+        echo '<div class="alert alert-success" role="alert">Your account has been verified successfully.</div>';
+        header('location:../index.php');
+      } else {
+        echo "<script>alert('Please fill all the fields')</script>";
+      }
+    }
+  }
+}
+?>
+
+
+
+<form method="POST" action="login.php">
+    <!-- Email input -->
+    <div class="form-outline mb-4">
+        <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
     </div>
-    <!-- Footer-->
-    <footer class="border-top">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <ul class="list-inline text-center">
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="small text-center text-muted fst-italic">
-                        Copyright &copy; Your Website 2022
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="../js/script.js"></script>
-</body>
 
-</html>
+    <!-- Password input -->
+    <div class="form-outline mb-4">
+        <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
+    </div>
+
+    <!-- Submit button -->
+    <button type="submit" name="submit" class="btn btn-primary mb-4 text-center">
+        Login
+    </button>
+
+    <!-- Register buttons -->
+    <div class="text-center">
+        <p>
+            a new member? Create an acount<a href="register.php"> Register</a>
+        </p>
+    </div>
+</form>
+<?php require '../includes/footer.php' ?>
