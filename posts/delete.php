f1a91d084f15cc79742f3d1263ba573647a183f7
RAKIB <rakibmahmudmridha@gmail.com>
2023-01-27 16:22:39 +0600

    delete file modified

--- posts/delete.php
+++ posts/delete.php
@@ -5,11 +5,15 @@
 if (isset($_GET['del_id'])) {
     $id = $_GET['del_id'];
 
+    $select = $conn->query("SELECT * FROM posts WHERE id = '$id'");
+    // $select->bindParam(':id', $id);
+    $select->execute();
+    $posts = $select->fetch(PDO::FETCH_OBJ);
+
+    unlink("images/" . $posts->img . "");
+
 
     $delete = $conn->prepare("DELETE FROM posts WHERE id = :id");
-    // $delete->execute([
-    //     ':id' => $id,
-    // ]);
 
     $delete->execute([
         ':id' => $id
